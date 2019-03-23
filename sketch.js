1cded2d6944882b7e02a7adc7ae987df111ea9bb
gekaremi <38594557+gekaremi@users.noreply.github.com>
2019-03-23 20:41:55 +0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -23,7 +23,7 @@ angleMode(RADIANS);
 
 function setup() {
   // createCanvas(710, 400);
-  createCanvas(0.99*windowWidth, 0.99*windowHeight);
+  createCanvas(windowWidth, windowHeight);
   pixelDensity(0.25);
   fill(128);
   base1 = createVector(0, height - 150);
